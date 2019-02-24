c5fd7aae696543154b0a887ed6c324218af69f01
Jeremiah Lowe <abc123me@github.com>
2019-02-24 02:18:42 +0000

    Moved module information to seperate file

---
+++ modinfo.h
@@ -0,0 +1,10 @@
+#ifndef _MODULE_INFO
+#define _MODULE_INFO
+
+#include "linux/module.h"
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Jeremiah Lowe");
+MODULE_DESCRIPTION("A module to control a chain of 74HC595 shift registers");
+
+#endif
