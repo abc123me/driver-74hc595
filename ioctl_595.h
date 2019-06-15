fa5cb1dc6730717d8a2751f34adacd32ba783ad5
abc123me <abc123me@github.com>
2019-06-15 05:02:45 +0000

    Changed uint8_t to be byte

--- ioctl_595.h
+++ ioctl_595.h
@@ -3,14 +3,10 @@
 
 #include "linux/ioctl.h"
 
-#ifndef uint8_t
-	typedef unsigned char uint8_t;
-#endif
-
 #define IOCTL_MAGIC 'k'
 #define IOCTL_RESET_595		_IO(IOCTL_MAGIC, 1)
-#define IOCTL_READ_CHAIN_LENGTH	_IOR(IOCTL_MAGIC, 2, uint8_t)
+#define IOCTL_READ_CHAIN_LENGTH	_IOR(IOCTL_MAGIC, 2, byte)
 #define IOCTL_MANUAL_LATCH	_IO(IOCTL_MAGIC, 3)
-#define IOCTL_SET_AUTO_LATCH	_IOW(IOCTL_MAGIC, 4, uint8_t)
+#define IOCTL_SET_AUTO_LATCH	_IOW(IOCTL_MAGIC, 4, byte)
 
 #endif
