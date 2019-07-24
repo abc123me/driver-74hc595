54d4c881ab8de44c307bf69ff2d29e31b2e97772
Jeremiah Lowe <8075565+abc123me@users.noreply.github.com>
2019-07-24 03:42:59 -0400

    Update updown.c

--- examples/updown.c
+++ examples/updown.c
@@ -5,19 +5,36 @@
 #include "stdint.h"
 #include "sys/ioctl.h"
 
+//Part of the driver's code, contains the ioctl calls needed to use the driver
+//https://github.com/abc123me/driver-74hc595/blob/master/ioctl_595.h
 #include "../ioctl_595.h"
 
-uint8_t getChainLength(int fd);
+/*
+This example utilizes a chain of 595's to march a bit along
+a row of LEDs, each LED is connected to the outputs of the
+shift registers sequentially, if wired correctly the first led will light up
+and march up and down the row of LEDs
+*/
+
+//Opens a file descriptor for the device
 int initFileDesc();
+//Returns the chain length via the ioctl
+uint8_t getChainLength(int fd);
+//Resets the entire chain of shift registers via an ioctl
 int resetChain(int fd);
 
+//For IOCTL information read here:
+//https://github.com/abc123me/driver-74hc595/blob/master/ioctl_595.h
+
 int main(int argc, char** argv){
+	//Initialize
 	uint16_t mdelay = 1000;
 	if(argc > 1){
 		mdelay = atoi(argv[1]);
 		printf("Delay set to %ims\n", mdelay);
 	}
-
+	
+	//Open and initialize the 595 device
 	int fd = initFileDesc();
 	if(fd < 0) return fd;
 	uint8_t chain_len = getChainLength(fd);
@@ -26,15 +43,19 @@ int main(int argc, char** argv){
 		return 1;
 	}
 
+	//Our animation, remember auto-latching is enabled by default
+	//so no manual latch is needed
 	uint16_t max = chain_len * 8;
 	uint64_t val = 1;
 	uint64_t udelay = 1000 * mdelay;
 	while(1){
+		//Move the bit left for each bit in the chain
 		for(uint8_t i = 0; i < max; i++){
 			val <<= 1;
 			write(fd, &val, chain_len);
 			usleep(udelay);
 		}
+		//Move the bit right for each bit in the chain
 		for(uint8_t i = 0; i < max; i++){
 			val >>= 1;
 			write(fd, &val, chain_len);
