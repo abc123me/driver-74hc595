45ca193da1592cd1d4bc73c0646638c3beff2443
abc123me <abc123me@github.com>
2022-10-09 22:35:08 -0400

    Invalid types

--- ioctl_595.h
+++ ioctl_595.h
@@ -2,11 +2,12 @@
 #define _IOCTL_H
 
 #include "linux/ioctl.h"
+#include "linux/types.h"
 
 #define IOCTL_MAGIC 'k'
 #define IOCTL_RESET_595		_IO(IOCTL_MAGIC, 1)
-#define IOCTL_READ_CHAIN_LENGTH	_IOR(IOCTL_MAGIC, 2, byte)
+#define IOCTL_READ_CHAIN_LENGTH	_IOR(IOCTL_MAGIC, 2, uint8_t)
 #define IOCTL_MANUAL_LATCH	_IO(IOCTL_MAGIC, 3)
-#define IOCTL_SET_AUTO_LATCH	_IOW(IOCTL_MAGIC, 4, byte)
+#define IOCTL_SET_AUTO_LATCH	_IOW(IOCTL_MAGIC, 4, uint8_t)
 
 #endif
